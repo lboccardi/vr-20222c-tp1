8e66918db6fa2350b40a8eaf334c2922ad25090e
alero <amoldovan@itba.edu.ar>
2022-08-23 17:02:58 -0300

    ChangeMaze (por cercanía y tecla E)

--- Assets/Scripts/ChangeMaze.cs
+++ Assets/Scripts/ChangeMaze.cs
@@ -6,6 +6,8 @@ public class ChangeMaze : MonoBehaviour
 {
     public GameObject maze_to_deactivate;
     public GameObject maze_to_activate;
+    public GameObject player;
+    public float distance; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,8 @@ public class ChangeMaze : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space")){
+        distance = Vector3.Distance(player.transform.position,transform.position);   
+        if (Vector3.Distance(player.transform.position,transform.position) < 1.3 && Input.GetKeyDown(KeyCode.E)){
             maze_to_deactivate.SetActive(false);
             maze_to_activate.SetActive(true);
         }
