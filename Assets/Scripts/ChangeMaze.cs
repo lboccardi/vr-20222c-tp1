f8523266b04370069d023bd4097c2de7511969e3
lboccardi <lboccardi@itba.edu.ar>
2022-08-25 15:26:36 -0300

    added rotation for Red Lever

--- Assets/Scripts/ChangeMaze.cs
+++ Assets/Scripts/ChangeMaze.cs
@@ -7,8 +7,16 @@ public class ChangeMaze : MonoBehaviour
     public List<GameObject> maze_to_deactivate;
     public List<GameObject> maze_to_activate;
     public GameObject player;
-    public float distance; 
+    public float distance;
+    public GameObject lever_handle;
     // Start is called before the first frame update
+    
+    private void SwitchHandle() {
+        Vector3 currentRotation = lever_handle.transform.localRotation.eulerAngles;
+        currentRotation.x = -currentRotation.x;
+        lever_handle.transform.localRotation = Quaternion.Euler(currentRotation);
+    }
+    
     void Start()
     {
         
@@ -19,6 +27,7 @@ public class ChangeMaze : MonoBehaviour
     {
         distance = Vector3.Distance(player.transform.position,transform.position);   
         if (Vector3.Distance(player.transform.position, transform.position) < 2 && Input.GetKeyDown(KeyCode.E)){
+            SwitchHandle();
             foreach (GameObject maze in maze_to_deactivate){
                 maze.SetActive(false);
             }
