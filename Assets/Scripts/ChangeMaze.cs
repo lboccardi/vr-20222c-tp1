fa87b2f79ab7547beb1e27572f1cebefbceae495
lboccardi <lboccardi@itba.edu.ar>
2022-08-25 16:32:34 -0300

    removed unnecessary gameobject and added Deactivate Behaviour

--- Assets/Scripts/ChangeMaze.cs
+++ Assets/Scripts/ChangeMaze.cs
@@ -8,13 +8,20 @@ public class ChangeMaze : MonoBehaviour
     public List<GameObject> maze_to_activate;
     public GameObject player;
     public float distance;
-    public GameObject lever_handle;
+    private bool fired = false;
     // Start is called before the first frame update
     
     private void SwitchHandle() {
-        Vector3 currentRotation = lever_handle.transform.localRotation.eulerAngles;
-        currentRotation.x = -currentRotation.x;
-        lever_handle.transform.localRotation = Quaternion.Euler(currentRotation);
+        Transform leverTrans = this.gameObject.transform.Find("Lever Handle");
+        if (leverTrans != null) {
+            Vector3 currentRotation = leverTrans.localRotation.eulerAngles;
+            currentRotation.x = -currentRotation.x;
+            leverTrans.transform.localRotation = Quaternion.Euler(currentRotation);
+        }
+    }
+
+    private void DeactivateLever() {
+        this.gameObject.SetActive(false);
     }
     
     void Start()
@@ -25,16 +32,19 @@ public class ChangeMaze : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(player.transform.position,transform.position);   
-        if (Vector3.Distance(player.transform.position, transform.position) < 2 && Input.GetKeyDown(KeyCode.E)){
-            SwitchHandle();
-            foreach (GameObject maze in maze_to_deactivate){
-                maze.SetActive(false);
-            }
-            foreach (GameObject maze in maze_to_activate){
-                maze.SetActive(true);
+        if (!fired) {
+            distance = Vector3.Distance(player.transform.position,transform.position);   
+            if (Vector3.Distance(player.transform.position, transform.position) < 2 && Input.GetKeyDown(KeyCode.E)){
+                SwitchHandle();
+                foreach (GameObject maze in maze_to_deactivate){
+                    maze.SetActive(false);
+                }
+                foreach (GameObject maze in maze_to_activate){
+                    maze.SetActive(true);
+                }
+                fired = true;
+                Invoke("DeactivateLever", 2);
             }
         }
-        
     }
 }
