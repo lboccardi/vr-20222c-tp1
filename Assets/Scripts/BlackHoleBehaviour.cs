151b36d26a27fd143c1c6c3b62795597b0ddae6f
Alejo <alejo.romano.98@gmail.com>
2022-08-26 12:32:55 -0300

    atraccion del agujero negro

--- Assets/Scripts/BlackHoleBehaviour.cs
+++ Assets/Scripts/BlackHoleBehaviour.cs
@@ -7,15 +7,39 @@ public class BlackHoleBehaviour : MonoBehaviour
     [SerializeField] private Vector3 _startPosition;
     [SerializeField] private float speed = 2.0f;
     
+    public GameObject player;
+    public GameObject ladder;
+    [SerializeField] private float _reachDistance = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         _startPosition = transform.position;    
     }
 
+    private bool IsObjectWithinPlayerReach()
+    {
+        //Ver de acceder a las dos primeras coordenadas X y Z de la escalera para hacer la distancia con eso y que no me tome la altura
+        float player_x = player.transform.position.x;
+        float player_z = player.transform.position.z;
+        float ladder_x = ladder.transform.position.x;
+        float ladder_z = ladder.transform.position.z;
+
+        Vector2 player_2d = new Vector2(player_x, player_z);
+        Vector2 ladder_2d = new Vector2(ladder_x, ladder_z);
+
+        return Vector2.Distance(player_2d, ladder_2d) < _reachDistance;
+        //return Vector3.Distance(player.transform.position, ladder.transform.position) < _reachDistance;
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.position = _startPosition + new Vector3(0.0f, 0.4f * Mathf.Sin(speed * Time.time), 0.0f);
+
+        if ( (IsObjectWithinPlayerReach()) && (player.transform.position.y < 5.5) )
+        {
+            player.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        }
     }
 }
